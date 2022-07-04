a9fc73c5dce4ff2908c4f51c3b3c00b42b7c8f4d
OfficerYoda <89543854+OfficerYoda@users.noreply.github.com>
2022-07-04 17:29:38 +0200

    Delete Tokens.java

--- src/de/officeryoda/Tokens.java
+++
@@ -1,8 +0,0 @@
-package de.officeryoda;
-
-public class Tokens {
-	public static final String BANE = "OTE4NTc2MDIzNDgwNzI1NTc2.G5dKLE.uypWDP7T8g5WO4V0UW5jVZlaeKz14NwHvtZAaI";
-	
-	public static final String BOBA = "OTIwNzEyOTAwMjkxNzU2MDY0.GiWaEz.SRh2pK5LoDmuvkbcwz__mS2jMiRrwTt-Y4kc5A";
-
-}
